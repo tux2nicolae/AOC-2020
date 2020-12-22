5ac27fdabd793da4b96f180460fd4923d702b689
Nicolae Telechi <nicolae.telechi@caphyon.com>
2020-12-22 23:43:21 +0200

    Prepare for day 23

---
+++ Day23/src/main.cpp
@@ -0,0 +1,46 @@
+/**
+ * Advent of code 2020
+ * @author : Nicolae Telechi
+ */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <optional>
+#include <numeric>
+#include <regex>
+#include <queue>
+#include <deque>
+
+#include <assert.h>
+
+using namespace std;
+
+#include "../../AOCLib/src/AOCLib.h"
+
+int main()
+{
+  ifstream in("..\\..\\Day23\\src\\_Input.in");
+  // ofstream out("..\\..\\Day23\\src\\_Output.out");
+
+  FStreamReader reader(in);
+  auto lines = reader.ReadLines();
+
+  for (auto& line : lines)
+  {
+  }
+
+  long long result = 0;
+  cout << result << endl;
+
+  return 0;
+}
