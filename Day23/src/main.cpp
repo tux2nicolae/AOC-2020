4f6c46f203ef81f8c15ec243b3f33931b886dc27
Nicolae Telechi <nicolae.telechi@caphyon.com>
2020-12-23 09:41:35 +0200

    Add solution for day 23

--- Day23/src/main.cpp
+++ Day23/src/main.cpp
@@ -27,20 +27,99 @@ using namespace std;
 
 #include "../../AOCLib/src/AOCLib.h"
 
+
+list<int>::iterator circularNext(list<int> & currentList, list<int>::iterator it)
+{
+  it++;
+  if (it == currentList.end())
+    it = begin(currentList);
+
+  return it;
+}
+
+
 int main()
 {
   ifstream in("..\\..\\Day23\\src\\_Input.in");
-  // ofstream out("..\\..\\Day23\\src\\_Output.out");
+  ofstream out("..\\..\\Day23\\src\\_Output.out");
 
   FStreamReader reader(in);
-  auto lines = reader.ReadLines();
+  auto line = reader.ReadLineAsDigits();
 
-  for (auto& line : lines)
+  list<int> circle;
+  vector<list<int>::iterator> previous(1000010, circle.end());
+
+  for (auto& digit : line)
   {
+    circle.push_back(digit);
+    previous[digit + 1] = prev(circle.end());
   }
 
-  long long result = 0;
-  cout << result << endl;
+  for (int digit = 10; digit <= 1000000; digit++)
+  {
+    circle.push_back(digit);
+    previous[digit + 1] = prev(circle.end());
+  }
+
+  // select destination cup
+  int minNumber = 1;
+  int maxNumber = circle.size();
+
+  auto currentCupIt = circle.begin();
+  auto destinationCupIt = circle.end();
+  auto previousDestinationCupIt = circle.end();
+
+  for (int iterations = 0; iterations < 10000000; iterations++)
+  {
+    vector<int> pickedUp;
+
+    // remove next 3
+    auto removeIt = circularNext(circle, currentCupIt);
+    for (int i = 0; i < 3; i++)
+    {
+      auto nextRemove = circularNext(circle, removeIt);
+      
+      pickedUp.push_back(*removeIt);
+      circle.erase(removeIt);
+
+      removeIt = nextRemove;
+    }
+
+    int destinationCup = *currentCupIt;
+    do {
+      destinationCup--;
+      if (destinationCup < minNumber)
+        destinationCup = maxNumber;
+    } while (find(begin(pickedUp), end(pickedUp), destinationCup) != end(pickedUp));
+
+    destinationCupIt = previous[destinationCup + 1];
+    
+    // insert numbers after destination cup
+    for (int i = pickedUp.size() - 1; i >= 0; i--)
+    {
+      auto insertNumber = pickedUp[i];
+      auto insertedIt = circle.insert(circularNext(circle, destinationCupIt), insertNumber);
+      previous[insertNumber + 1] = insertedIt;
+    }
+
+    // move current list
+    currentCupIt = circularNext(circle, currentCupIt);
+  }
+
+
+  auto oneCup = find(begin(circle), end(circle), 1);
+  // for (auto it = circularNext(circle, oneCup); it != oneCup; it = circularNext(circle, it))
+  // {
+  //   cout << *it << " ";
+  // }
+
+  cout << endl;
+  cout << *circularNext(circle, oneCup) << endl;
+  cout << *circularNext(circle, circularNext(circle, oneCup)) << endl;
+
+  cout << endl;
+  cout << long long(*circularNext(circle, oneCup)) * *circularNext(circle, circularNext(circle, oneCup)) << endl;
+
 
   return 0;
 }
